153bb51a7382ed5b2b2b1056d658061e6ed2092b
Nikhil Arsid <128669883+nikhilarsid@users.noreply.github.com>
2025-08-10 18:33:05 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -3,8 +3,7 @@
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-# --- 1. Define Database Configuration ---
-# This will create a file named 'inventory.db' in our project folder.
+
 DATABASE_URL = "sqlite:///inventory.db"
 # The 'engine' is the entry point to our database.
 engine = create_engine(DATABASE_URL)
@@ -19,11 +18,7 @@ except FileNotFoundError:
     print("❌ Error: sales_data.csv not found. Cannot initialize database.")
     exit()
 
-# --- 3. Store Data in the Database ---
-# We'll name our table 'sales'.
-# 'if_exists='replace'' means if the table already exists, it will be dropped and recreated.
-# This is useful for re-running the script, but be careful in a real production environment!
-# 'index=False' means we won't write the pandas DataFrame index as a column in the table.
+
 try:
     with engine.connect() as connection:
         df.to_sql('sales', con=connection, if_exists='replace', index=False)
